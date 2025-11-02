7ceff0e6c630ec8e2e1426ec6bbed8ecf623407c
adria <adriacortzss@gmail.com>
2025-11-02 15:07:13 -0300

    Correct code

--- app.jsx
+++ app.jsx
@@ -1,28 +1,45 @@
-
 //VOu comentar algumas coisas pra não me perder e não fazer tudo mal feito
 
-function Nav(){
-
-return(<div className="navbar">
-  <div className="flex">/
-    <a className="btn">botao</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="bg">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>
-) 
+function Nav() {
+  return (
+    <div className="navbar">
+      <div className="dropdown dropdown-end">
+        <div
+          tabIndex={-1}
+          role="button"
+          className="btn btn-ghost rounded-field"
+        >
+          . . .
+        </div>
+        <ul
+          tabIndex="-1"
+          className="menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Soft Skills</a>
+          </li>
+          <li>
+            <a href="#">Hard Skills</a>
+          </li>
+          <li>
+            <a href="#">Enterprises</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Let me impress you</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 function Cabecalho() {
@@ -33,16 +50,15 @@ function Cabecalho() {
   );
 }
 
-//Esses aqui vão renderizar na tela. 
+//Esses aqui vão renderizar na tela.
 
-const rootElement = document.getElementById('rootElement'); //Renderiza o cabeçalho
-const navElement = document.getElementById('navElement'); //renderiza o menu
+const rootElement = document.getElementById("rootElement"); //Renderiza o cabeçalho
+const navElement = document.getElementById("navElement"); //renderiza o menu
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<Cabecalho />);
 }
 
-
-if (navElement) {   
- ReactDOM.createRoot(navElement).render(<Nav />);
+if (navElement) {
+  ReactDOM.createRoot(navElement).render(<Nav />);
 }
